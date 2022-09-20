0c5b5836661350bea941164ad20e7b677ae0cb25
Maxwell Moreira <maxwellburkemoreira@gmail.com>
2022-09-20 20:51:26 -0300

    Adicionado scripts locais, config inicial dat.GUI

---
+++ resources/gui.js
@@ -0,0 +1,21 @@
+const objeto1 = {
+  animate: true,
+  speed : 3,
+  cameraPosX: 4,
+  cameraPosY: 3.5,
+  cameraPosZ: 10
+};
+
+
+
+
+const loadGUI = () => {
+  const gui = new dat.GUI();
+  gui.add(objeto1, "animate");
+  gui.add(objeto1,"speed", 0, 5, 0.1);
+  gui.add(objeto1, "cameraPosX",-30, 30,0.1);
+  gui.add(objeto1,"cameraPosY",-30, 30, 0.1);
+  gui.add(objeto1,"cameraPosZ",-30,30,0.1);
+};
+
+
